488f2eaa0a60d73a9c05b93327776fdf28e8de25
skihumba <abmuhiks@gmail.com>
2018-07-30 23:56:05 +0300

    completed task1 and task2

--- task1.py
+++ task1.py
@@ -2,8 +2,13 @@ import random
 
 ourList = list()
 count = 0 
-while (count < 11):
+while (count < 10):
     ourList.append(random.randint(1,10))
     count += 1
     
-ourList
+print(ourList)
+
+#get list of rlrmrnts in ourList that are < 5
+b5 = [i for i in ourList if i < 5]
+
+print(b5)
--- task2.py
+++ task2.py
@@ -3,4 +3,9 @@ import random
 words = [word.rstrip('\n') for word in open('words.txt')]
 randomPhrase = " ".join([words[random.randrange(0, len(words))] for i in range(4)])
 
-randomPhrase
+print(randomPhrase)
+
+#reverse words and letters in randonPhrase
+reversePhrase = randomPhrase[::-1]
+
+print(reversePhrase)
